4fbcad9a151199152b2942f8fb21c68414e3182f
brody <youoper@gmail.com>
2020-12-29 22:18:07 +0900

    test: MemberService 통합 Test 추가

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 
 @SpringBootTest
 @Transactional
@@ -17,14 +19,31 @@ class MemberServiceTest {
     @Autowired MemberRepository memberRepository;
 
     @Test
-    void 회원가입() throws Exception{
-
+    void 회원가입() throws Exception {
+        // given
         Member member = new Member();
         member.setName("Kim");
 
+        // when
         Long memberId = memberService.join(member);
 
+        // then
         Assertions.assertThat(member).isEqualTo(memberService.findOne(memberId));
+    }
 
+    @Test
+    public void 중복_회원_예외() throws Exception {
+        // given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+        // when
+        memberService.join(member1);
+
+        // then
+        assertThrows(IllegalStateException.class,
+                () -> memberService.join(member2));
     }
 }
