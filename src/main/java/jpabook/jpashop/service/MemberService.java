6de398c0471b92a37006e50ccfb94d154bbb5cac
brody <youoper@gmail.com>
2020-12-29 21:41:42 +0900

    style: MemberService 주석 추가

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -15,6 +15,9 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    /**
+     * 회원 가입
+     */
     @Transactional
     public Long join(Member member) {
         validateDuplicateMember(member);
@@ -29,6 +32,9 @@ public class MemberService {
         }
     }
 
+    /**
+     * 회원 전체 조회
+     */
     public List<Member> findAll(){
         return memberRepository.findAll();
     }
